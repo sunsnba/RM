9fb28e8b20b0e0f243daced8519e2cbcee5a3311
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2018-12-06 15:14:15 -0800

    header button now functional

--- src/App.js
+++ src/App.js
@@ -13,14 +13,22 @@ class App extends Component {
     hData: data.headerData,
     contA: data.contentA,
     contB: data.contentB,
-  }    
+    isPositive: false,
+  }
+  this.headerClick = this.headerClick.bind(this);    
+  }
+
+  headerClick = () => {
+    this.setState(prevState => ({
+      isPositive: !prevState.isPositive,
+    }));
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-         <Header hData={this.state.hData}/>
+         <Header hData={this.state.hData} headerClick={this.headerClick} isPositive={this.state.isPositive}/>
         </header>
         <PanelA />
         <PanelB />
